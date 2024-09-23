af96c00754492e02cec8e37dea76b11f6a4579b4
Shayerine <lordacaelusmuintir@hotmail.com>
2024-09-23 01:23:07 -0500

    Add sign-in/sign-up components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import { Button } from "@/components/ui/button";
+import { AuthScreen } from "@/features/auth/components/auth-screen";
 
 export default function Home() {
   return (
-    <Button>Click Me</Button>
+    <AuthScreen />
   );
 }
