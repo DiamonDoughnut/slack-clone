b540f085c709b2475664f295d78e72bdf58e7575
Shayerine <lordacaelusmuintir@hotmail.com>
2024-09-30 19:07:50 -0500

    Implement message API to bring messages to front end with type safety

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -7,11 +7,14 @@ import { useGetChannel } from "@/features/channels/api/use-get-channel";
 import { useChannelId } from "@/hooks/useChannelId";
 import { Header } from "./header";
 import { ChatInput } from "./chat-input";
+import { useGetMessages } from "@/features/messages/api/use-get-messages";
 
 const ChannelIdPage = () => {
     const channelId = useChannelId();
 
+    const { results } = useGetMessages({ channelId });
     const { data: channel, isLoading: channelLoading } = useGetChannel({ id: channelId });
+    console.log(results);
 
     if (channelLoading) {
         return (
@@ -25,7 +28,7 @@ const ChannelIdPage = () => {
         return (
             <div className="h-full flex-1 flex flex-col gap-y-2 items-center justify-center">
                 <TriangleAlert className="size-6 text-muted-foreground" />
-                <span className="text-small text-muted-foregroun">
+                <span className="text-small text-muted-foreground">
                     Channel Not Found
                 </span>
             </div>
@@ -39,7 +42,9 @@ const ChannelIdPage = () => {
               className="
                 flex-1
               "
-            />
+            >
+               {JSON.stringify(results)} 
+            </div>
             <ChatInput placeholder={`message #${channel.name}`} />
         </div>    
      );
