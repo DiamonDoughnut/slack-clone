cdd8b946c3c7399d73c3dc5245cf31cb898b5575
Shayerine <lordacaelusmuintir@hotmail.com>
2024-10-01 19:43:54 -0500

    begin implementation of DM system

---
+++ src/app/workspace/[workspaceId]/member/[memberId]/page.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import { AlertTriangle, Loader } from "lucide-react";
+import { useEffect, useState } from "react";
+
+import { useCreateOrGetConversation } from "@/features/conversations/api/use-create-or-get-conversation";
+
+import { useMemberId } from "@/hooks/useMemberId";
+import { useWorkspaceId } from "@/hooks/useWorkspaceId";
+import { Id } from "../../../../../../convex/_generated/dataModel";
+import { toast } from "sonner";
+import { Conversation } from "./conversation";
+
+const MemberIdPage = () => {
+    const workspaceId = useWorkspaceId();
+    const memberId = useMemberId();
+
+    const [conversationId, setConversationId] = useState<Id<'conversations'> | null>(null)
+
+    const { data, mutate, isPending } = useCreateOrGetConversation();
+
+
+    useEffect(() => {
+        mutate({
+            workspaceId,
+            memberId
+        }, {
+            onSuccess(data) {
+                setConversationId(data)
+            },
+            onError(_error) {
+                toast.error('Failed to Retrieve Conversation')
+            }
+        }
+    )
+    }, [memberId, workspaceId, mutate])
+
+    if (isPending) {
+        return (
+            <div className="h-full flex items-center justify-center">
+                <Loader className="size-6 animate-spin text-muted-foreground" />
+            </div>
+        )
+    }
+    
+    if (!conversationId) {
+        return (
+            <div className="h-full flex flex-col gap-y-2 items-center justify-center">
+                <AlertTriangle className="size-6 text-muted-foreground" />
+                <span className="text-small text-muted-foreground">
+                    Conversation Not Found
+                </span>
+            </div>
+        )
+    }
+
+    return <Conversation id={conversationId} />;
+}
+ 
+export default MemberIdPage;
