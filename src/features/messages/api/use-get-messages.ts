3fe3f5745ffe51347a44a758973e902541f43612
Shayerine <lordacaelusmuintir@hotmail.com>
2024-10-04 15:35:07 -0500

    explain error expecting

--- src/features/messages/api/use-get-messages.ts
+++ src/features/messages/api/use-get-messages.ts
@@ -10,7 +10,7 @@ interface UseGetMessagesProps {
     conversationId?: Id<'conversations'>
     parentMessageId?: Id<'messages'>
 }
-//@ts-expect-error
+//@ts-expect-error pagination return type doesn't implicitly have 'page' property, but does after pagination
 export type GetMessagesReturnType = typeof api.messages.get._returnType["page"];
 
 export const useGetMessages = ({
@@ -19,7 +19,7 @@ export const useGetMessages = ({
     parentMessageId
 }: UseGetMessagesProps) => {
     const { results, status, loadMore } = usePaginatedQuery(
-        //@ts-expect-error
+        //@ts-expect-error pagination return type doesn't implicitly have 'page' property, but does after pagination
         api.messages.get,
         { channelId, conversationId, parentMessageId },
         { initialNumItems: BATCH_SIZE }
